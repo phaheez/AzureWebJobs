42ced35126199ce69a1526cfc0dea4f4a4d0d9f7
phaheez <faizfasasi@gmail.com>
2022-02-24 00:56:58 +0100

    Add project files.

---
+++ QueueProcessor/Functions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueProcessor
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([QueueTrigger("webjobqueue")] string message, ILogger logger)
+        {
+            logger.LogInformation(message);
+        }
+    }
+}
